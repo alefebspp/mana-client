7dfaf17a034c525b902640d5e934155ce1712208
Alefe Bispo <alefe.bispo@onesight.com.br>
2023-12-31 22:30:38 -0300

    add options to select test

--- src/components/Select/test.tsx
+++ src/components/Select/test.tsx
@@ -6,7 +6,7 @@ const mountComponent = () => {
   return (
     <Select.Container>
       <Select.Label>Label</Select.Label>
-      <Select.Root name="test" />
+      <Select.Root options={[]} name="test" />
     </Select.Container>
   )
 }
